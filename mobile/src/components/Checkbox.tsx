b8ebb96442ffc66cdb5d28b8525639c3d4c2a4d6
Rodrigo Bonatti <bonattirodrigo123@hotmail.com>
2023-01-19 22:40:41 -0800

    Implement routes

---
+++ mobile/src/components/Checkbox.tsx
@@ -0,0 +1,10 @@
+import { TouchableOpacity } from 'react-native';
+
+export function Checkbox() {
+  return (
+    <TouchableOpacity
+      activeOpacity={0.7}
+      className="flex-row mb-2 items-center"
+    ></TouchableOpacity>
+  );
+}
